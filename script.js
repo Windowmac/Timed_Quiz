25291ce7a2a08dcbe1e700b0021ef9adacd2de5b
McKendree Strommer <mckendree.strommer@gmail.com>
2021-05-30 21:39:00 -0700

    refined code to no errors - all that remains is local storage form input

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const questionArray = [
   },
 ];
 
-//set timer to 2 minutes.
+//set timer to 60 seconds.
 //user clicks on start button, 1st question shows up in questions container, countdown begins
 //user clicks on question - message displays confirming correct or incorrect over question content
 //if correct, display 'correct! (please click)' in #question, check if there are any more questions to display, then display next question.
@@ -52,47 +52,83 @@ let questionCount = 0;
 const startButtonEl = document.getElementById('start-button');
 const questionEl = document.getElementById('question');
 
-function startCountdown() {
-  const timeInterval = setInterval(handleInterval, 1000);
-  function handleInterval() {
-    timeLeft--;
-    countdownEl.textContent = `${timeLeft} seconds left`;
-    if (timeLeft <= 0) {
-      clearInterval(timeInterval);
-      endGame();
-    }
-  }
-}
-
-function handleQuestionClick(event) {
-  const userChoice = event.target.textContent;
-  const correctChoice = questionArray[questionCount].correctAnswer;
-  if (userChoice === correctChoice) {
-    questionCount++;
-    if (questionCount > questionArray.length) {
-      endGame();
-    }
-    startQuestions();
-  } else {
-    timeLeft = timeLeft - 5;
-    startQuestions();
-  }
-}
-
-function startQuestions() {
-  questionEl.textContent = questionArray[questionCount].textOfQuestion;
-  for (let i = 0; i < questionArray[questionCount].options.length; i++) {
-    let option = document.createElement('li');
-    option.textContent = questionArray[questionCount].options[i];
-    questionEl.appendChild(option);
-    option.addEventListener('click', handleQuestionClick);
-  }
-}
+const colorTextEl = document.getElementById('color-text');
+const announcementEl = document.getElementById('announcement');
+const listItems = document.getElementsByTagName('li');
 
 function handleStartButton(event) {
   startCountdown();
-  startButtonEl.remove();
+
+  function startCountdown() {
+    const timeInterval = setInterval(handleInterval, 1000);
+    function handleInterval() {
+      timeLeft--;
+      countdownEl.textContent = `${timeLeft} seconds left`;
+      if (timeLeft <= 0) {
+        clearInterval(timeInterval);
+        endGame(timeLeft);
+      }
+    }
+  }
   startQuestions();
+  startButtonEl.remove();
+
+  function startQuestions() {
+    if (questionArray[questionCount]) {
+      questionEl.textContent = questionArray[questionCount].textOfQuestion;
+      for (let i = 0; i < questionArray[questionCount].options.length; i++) {
+        let option = document.createElement('li');
+        option.textContent = questionArray[questionCount].options[i];
+        questionEl.appendChild(option);
+        option.addEventListener('click', handleQuestionClick);
+
+        function handleQuestionClick(event) {
+          if (questionCount > questionArray.length - 1 || timeLeft <= 0) {
+            endGame(timeLeft);
+          }
+
+          const userChoice = event.target.textContent;
+          const correctChoice = questionArray[questionCount].correctAnswer;
+
+          if (userChoice === correctChoice) {
+            questionCount++;
+            for (let i = 0; i < listItems.length; i++) {
+              console.log(listItems[i]);
+            }
+            colorTextEl.textContent = 'Correct! ';
+            colorTextEl.style.color = 'blue';
+            announcementEl.textContent = 'Next question: ';
+            startQuestions();
+          } else {
+            timeLeft = timeLeft - 5;
+            colorTextEl.textContent = 'Incorrect! ';
+            colorTextEl.style.color = 'red';
+            announcementEl.textContent = '-5 seconds, repeat question: ';
+            startQuestions();
+          }
+        }
+      }
+    } else {
+      endGame(timeLeft);
+    }
+
+    function endGame(timeLeft) {
+      let score = timeLeft * 100;
+      if (score < 0) {
+        score = 0;
+      }
+
+      countdownEl.remove();
+
+      colorTextEl.textContent = 'Congratulations!';
+      colorTextEl.style.color = 'green';
+      announcementEl.textContent = 'your score is: ' + score;
+      console.log(announcementEl);
+      console.log(score);
+
+      questionEl.remove();
+    }
+  }
 }
 
 startButtonEl.addEventListener('click', handleStartButton);
